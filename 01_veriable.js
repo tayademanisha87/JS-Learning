dc5c564ec3ab88be4a3f8ddb260a5ef8053377be
tayademani <mwankhedkar87@gmail.com>
2023-07-19 22:58:26 +0530

    02 Assign
    Done

---
+++ 01_veriable.js
@@ -0,0 +1,10 @@
+var myFirstName; // Variable Declaration
+myName = "Manisha"; // Variable Assignment or Initialization
+
+console.log(myName);
+
+var city = "Pune"; // Variable Declaration and Initialization
+console.log(city);
+
+var pin; // Variable Declaration
+console.log(pin);
---
+++ 02_dataType.js
@@ -0,0 +1,27 @@
+var bankName
+console.log(bankName);
+
+var bankName ="Bank Name: SBI Bank"
+console.log(bankName);
+console.log(typeof bankName);
+
+var marks ="Marks: 90%"
+console.log(marks);
+console.log(typeof marks);
+
+var isWorking =  true
+console.log(isWorking);
+console.log(typeof isWorking);
+
+var totalCount = "Hundred and Seven"
+console.log(totalCount);
+console.log(typeof totalCount);
+
+var totalCount=107
+console.log(totalCount);
+console.log(typeof totalCount);
+
+
+
+
+    
